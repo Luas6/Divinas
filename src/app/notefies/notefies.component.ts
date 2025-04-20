5aeded1ba2758089e4b7bf613eda3142574c710b
Saul Matías Jiménez <saulmaji@gmail.com>
2025-04-21 00:06:08 +0200

    No te fíes + Home

--- src/app/notefies/notefies.component.ts
+++ src/app/notefies/notefies.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./notefies.component.css']
 })
 export class NotefiesComponent {
+  customOptions = {
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    dotsEach: 1,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      992: {
+        items: 3,
+        autoplay: false,
+        dots: false,
+        loop: false,
 
+      }
+    }
+  }
 }
